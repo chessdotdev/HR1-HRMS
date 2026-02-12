9b1c10ac1b879a29b5f4556ed535ff56297ff2fe
Robert Pradilla <robertpradilla03@gmail.com>
2026-02-12 04:34:32 -0800

    bootstrap

--- includes/footer.php
+++ includes/footer.php
@@ -2,10 +2,8 @@
 
 
  <!-- Bootstrap JS Bundle (includes Popper) -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qDr8fOybO0Z+XkEwXwL8Rn3G6pJQ/5w0+0Btw3RzR9lHnKdT0vL0yYgZ+0Q9f9N9" crossorigin="anonymous"></script>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-<script src="../publc/assets/js/bootstrap.bundle.min.js"></script> <!-- typo: publc -->
+<script src="../publc/assets/js/bootstrap.bundle.min.js"></script> 
 
 </body>
 </html>
